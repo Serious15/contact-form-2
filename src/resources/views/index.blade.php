e4de276335b24be7f2e3db1482348e91483a02b9
Yuka <atemu.love@gmail.com>
2025-02-25 18:37:23 +0900

    commit

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FashionablyLate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">
+                FashionablyLate
+            </a>
+        </div>
+    </header>
+
+    <main>
+        <div class="contact-form__content">
+            <div class="contact-form__heading">
+                <h2>Contact</h2>
+            </div>
+            <form class="form" action="/contacts/confirm" method="post">
+                @csrf
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">お名前*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="text" name="first_name" placeholder="山田" value="{{ old('first_name') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="text" name="last_name" placeholder="太郎" value="{{ old('last_name') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">性別*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="tinyint" name="gender" placeholder="1:男性,2;女性,3:その他" value="{{ old('gender') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">メールアドレス*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="varchar" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">電話番号*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="varchar" name="tel" placeholder="08012345678" value="{{ old('tel') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">住所*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="varchar" name="address" placeholder="例:東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('address') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">建物名*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="varchar" name="building" placeholder="例:千駄ヶ谷マンション101" value="{{ old('building') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">お問い合わせの種類*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="text" name="detail" placeholder="選択してください" value="{{ old('detail') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">お問い合わせ内容*</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--textarea">
+                            <textarea name="content" placeholder="お問い合わせ内容をご記載ください"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="form__button">
+                    <button class="form__button-submit" type="submit">確認画面</button>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+
+</html>
